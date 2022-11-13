71cfe5d4b7633b8f9258533b101439685672efd0
Temesgen Tsegaye <temesgentsegaye9891@gmail.com>
2022-11-13 23:32:29 +0300

    add projects

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,77 +1,192 @@
 import { gsap } from "gsap";
-import {useRef,useLayoutEffect} from 'react'
+import { useRef, useLayoutEffect } from "react";
 import ScrollTrigger from "gsap/ScrollTrigger";
-   
-gsap.registerPlugin(ScrollTrigger)
 
-const Project=()=>{
-    const pro1=useRef(null)
-    const pro1l=useRef(null)
-    // const pro2=useRef(null)
+gsap.registerPlugin(ScrollTrigger);
+
+const Project = () => {
+  const pro1 = useRef(null);
+ 
+  const pro2=useRef(null)
+
+  const pro3=useRef(null)
+  const pro4=useRef(null)
+  const pro5=useRef(null)
+
+  useLayoutEffect(() => {
     
-    // const pro3=useRef(null)
-    // const pro4=useRef(null)
-    // const pro5=useRef(null)
+    let ctx = gsap.context(() => {
+      gsap.from(".sleft1", {
+        xPercent: -100,
+        duration: 1,
+        scale: 0.5,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro1.current,
 
-    useLayoutEffect(()=>{
-        let ctx=gsap.context(()=>{
-            gsap.to(pro1l.current,{backgroundColor:'red',duration:2,scrollTrigger:{
-                trigger:pro1.current,
-                
-                start:'top bottom',
-                toggleActions:'restart reverse restart reverse'
-            }})
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+      gsap.from(".sright1", {
+        xPercent: 100,
+        scale: 0.5,
+        duration: 1,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro1.current,
 
-        },pro1)
-        return () => ctx.revert();
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+    }, pro1);
+    return () => ctx.revert();
+  }, []);
+  useLayoutEffect(() => {
+    
+    let ctx = gsap.context(() => {
+      gsap.from(".sleft2", {
+        xPercent: -100,
+        duration: 1,
+        scale: 0.5,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro2.current,
 
-    },[])
-    return(
-        // <div className='project'>
-           <div ref={pro1} className='project'>
-           <div className='projectslider'>
-                <div className='sleft1' ref={pro1l} ></div>
-                <div className='sright1'></div>
-                </div>
- {/* <div ref={pro2}>
-           <div className='projectslider2'>
-                  <div className='sleft2'></div>
-                <div className='sright2'></div>
-                
-            </div>
-           </div>
-         
-           <div ref={pro3}>
-           <div className='projectslider'>
-                 <div className='sleft'></div>
-                <div className='sright'></div>
-                </div>
-           </div>
-           
-            
-         <div ref={pro4}>
-         <div className='projectslider'>
-               <div className='sleft'></div>
-                <div className='sright'></div>
-                </div>
-         </div>
-        
-            
-           <div ref={pro5}>
-           <div className='projectslider'>
-                 <div className='sleft'></div>
-                <div className='sright'></div>
-            
-            </div>
-           </div>
-           */}
-           </div>
-               
-                
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+      gsap.from(".sright2", {
+        xPercent: 100,
+        scale: 0.5,
+        duration: 1,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro2.current,
 
-          
-            
-        // </div>
-    )
-}
-export default Project
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+    }, pro2);
+    return () => ctx.revert();
+  }, []);
+  useLayoutEffect(() => {
+    
+    let ctx = gsap.context(() => {
+      gsap.from(".sleft3", {
+        xPercent: -100,
+        duration: 1,
+        scale: 0.5,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro3.current,
+
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+      gsap.from(".sright3", {
+        xPercent: 100,
+        scale: 0.5,
+        duration: 1,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro3.current,
+
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+    }, pro3);
+    return () => ctx.revert();
+  }, []);
+  useLayoutEffect(() => {
+    
+    let ctx = gsap.context(() => {
+      gsap.from(".sleft4", {
+        xPercent: -100,
+        duration: 1,
+        scale: 0.5,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro4.current,
+
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+      gsap.from(".sright4", {
+        xPercent: 100,
+        scale: 0.5,
+        duration: 1,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro4.current,
+
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+    }, pro4);
+    return () => ctx.revert();
+  }, []);
+  useLayoutEffect(() => {
+    
+    let ctx = gsap.context(() => {
+      gsap.from(".sleft5", {
+        xPercent: -100,
+        duration: 1,
+        scale: 0.5,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro5.current,
+
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+      gsap.from(".sright5", {
+        xPercent: 100,
+        scale: 0.5,
+        duration: 1,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: pro5.current,
+
+          start: "top center",
+          toggleActions: "restart reverse restart reverse",
+        },
+      });
+    }, pro5);
+    return () => ctx.revert();
+  }, []);
+  
+  return (
+      <>
+    <div ref={pro1} className="project">
+      <div className="sleft sleft1" ></div>
+      <div className="sright sright1"></div>
+    </div>
+    <div ref={pro2} className="project">
+      <div className="sleft sleft2" ></div>
+      <div className="sright sright2"></div>
+    </div>
+    <div ref={pro3} className="project">
+      <div className="sleft sleft3" ></div>
+      <div className="sright sright3"></div>
+    </div>
+    <div ref={pro4} className="project">
+      <div className="sleft sleft4" ></div>
+      <div className="sright sright4"></div>
+    </div>
+    <div ref={pro5} className="project">
+      <div className="sleft sleft5" ></div>
+      <div className="sright sright5"></div>
+    </div>
+    </>
+  );
+};
+export default Project;
